20dd09308cbfbe66f5e2a92f493f3bb731a8c1ec
Christel Trutmann <ctrutmann@gmail.com>
2016-08-10 13:35:47 -0700

    Add password and password_confirmation to permitted user_params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    flash = {succes: 'You have logged in successfully', danger: 'Invalid email/password combination'}
+
     if user && user.authenticate(params[:session][:password])
       log_in user
       # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      flash[:success] = 'You have logged in successfully'
+      # flash[:success] = 'You have logged in successfully'
+      flash[:success]
       redirect_back_or user
     else
       # flash[:danger] = 'Invalid email/password combination'
+      flash[:danger]
       render 'new'
     end
   end
