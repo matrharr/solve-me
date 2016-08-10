dae8eecaca7485dbfcb11c2d12bd86392401a3c8
Christel Trutmann <ctrutmann@gmail.com>
2016-08-10 14:11:57 -0700

    Fix typo in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmationC)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 
