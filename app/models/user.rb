0c626d22d122c9dcc547a1fc7d72614180174c52
Christel Trutmann <ctrutmann@gmail.com>
2016-08-10 10:05:41 -0700

    Add has_secure_password validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ApplicationRecord
-
-
   validates_presence_of :first_name, :last_name, :email
 
   has_one :questionaire
-  # has_many :questionaire_takers, through: :questionaire_takers_matches, source: :matches
-  # has_many :questionaire_makers, through: :questionaire_makers_matches, source: :matches
- 
+
   has_many :questionaire_takers, through: :taker_matches, source: :questionaire_taker
   has_many :taker_matches, foreign_key: :questionaire_maker_id, class_name: "Match"
 
   has_many :questionaire_makers, through: :maker_matches, source: :questionaire_maker
   has_many :maker_matches, foreign_key: :questionaire_taker_id, class_name: "Match"
 
+  has_secure_password
 
 end
