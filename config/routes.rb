c4d3cee127613ba8778f254d70a264342eb235df
Christel Trutmann <ctrutmann@gmail.com>
2016-08-11 21:42:15 -0700

    Update config/routes.rb to include verify and users#edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get    'verify'  => 'sessions#verify_access_token'
 
-  resources :users
+  get 'users/:access_token/edit' => 'users#edit'
+
+  resources :users, param: :access_token
   resources :questionaires
   resources :matches, only: [:index, :delete]
 
