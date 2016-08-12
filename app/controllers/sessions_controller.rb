196af8c325280172d8ee645c47c7bfb42e216c1b
Christel Trutmann <ctrutmann@gmail.com>
2016-08-11 21:40:27 -0700

    Update SessionsController to #verify_access_token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,25 @@ class SessionsController < ApplicationController
   def new
   end
 
+  #If user login data are valid it will return the access_token so the
+  #client app can use it for future request for the specific user.
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    flash = {succes: 'You have logged in successfully', danger: 'Invalid email/password combination'}
+      if user && user.authenticate(params[:session][:password])
+        render :text => user.access_token, status: 200
+      else
+        render text: "Email and password combination are invalid", status: 422
+      end
+  end
 
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # flash[:success] = 'You have logged in successfully'
-      flash[:success]
-      redirect_back_or user
-    else
-      # flash[:danger] = 'Invalid email/password combination'
-      flash[:danger]
-      render 'new'
-    end
+  #Verifies the access_token so the client app would know if to login the user.
+  def verify_access_token
+    user = User.find_by(access_token: params[:session][:access_token])
+      if user
+        render text: "verified", status: 200
+      else
+        render text: "Token failed verification", status: 422
+      end
   end
 
   def destroy
